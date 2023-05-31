fadf66dece988a57bbe16527feef241e0ab0b339
PabloMartinsBorges <51960948+PabloMartinsBorges@users.noreply.github.com>
2023-05-31 17:25:14 -0300

    teste do exercicio 2 com strings

--- Conjunto/src/exercicio2/Teste.java
+++ Conjunto/src/exercicio2/Teste.java
@@ -20,6 +20,8 @@ public class Teste {
 		System.out.println("Imprimindo c1");
 		c1.imprime();
 
+		System.out.println();
+		
 		c2.insere(1);
 		c2.insere(3);
 		c2.insere(5);
@@ -29,16 +31,60 @@ public class Teste {
 
 		System.out.println("Imprimindo c2");
 		c2.imprime();
+		System.out.println();
 		
-		System.out.println("E igual: " + c1.eIgual(c2));
+		System.out.println("E igual: " + c1.eIgual(c2)+ "\n");
+		
 		
 		c2.remove(9);
-		System.out.println("Elemento '9' removido do conjunto 2");
+		System.out.println("Elemento '9' removido do conjunto 2 \n");
 		
+		System.out.println("Imprimindo c1");
 		c1.imprime();
+		
+		System.out.println("Imprimindo c2");
 		c2.imprime();
-		System.out.println("E igual:  " + c1.eIgual(c2));
+		
+		System.out.println("E igual:  " + c1.eIgual(c2)+ "\n");
+		
+		
+		System.out.println("---------------------");
+		System.out.println("Teste Com Strings");
+		System.out.println();
+		
+		
+		ConjGenerico<String> c3, c4;
+		
+		c3 = new ConjGenerico<String>();
+		c4 = new ConjGenerico<String>();
 
+		c3.insere("ab");
+		c3.insere("bc");
+		c3.insere("cd");
+		c3.insere("be");
+		
+		System.out.println("Imprimindo c3");
+		c3.imprime();
+		
+		c4.insere("ab");
+		c4.insere("cd");
+		c4.insere("bc");
+		
+		System.out.println("Imprimindo c4");
+		c4.imprime();
+		
+		System.out.println("E igual: " + c3.eIgual(c4)+ "\n");
+		
+		System.out.println("Removendo 'be' de c3 \n");
+		c3.remove("be");
+		
+		System.out.println("Imprimindo c3");
+		c3.imprime();
+		
+		System.out.println("Imprimindo c4");
+		c4.imprime();
+		
+		System.out.println("E igual: " + c3.eIgual(c4)+ "\n");
 		
 	}
 }
